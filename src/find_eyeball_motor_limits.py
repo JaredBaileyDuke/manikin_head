3987a84eef586bf9542a9c028d5597a0cd9444b1
Jared Bailey <Jared.L.Bailey@duke.edu>
2025-09-27 16:24:22 -0400

    Found eyeball and eyelid limits

--- src/find_eyeball_motor_limits.py
+++ src/find_eyeball_motor_limits.py
@@ -5,18 +5,18 @@ from adafruit_servokit import ServoKit
 kit = ServoKit(channels=16)
 
 # Servo on channel x
-servo = kit.servo[3]
+servo = kit.servo[1]
 
 # Limit the pulse width range to better match servos
 servo.set_pulse_width_range(500, 2500)
 
 while True:
-    servo.angle = 35
-    sleep(1)
+    # servo.angle = 60
+    # sleep(1)
+    # servo.angle = 160
+    # sleep(1)
     servo.angle = 125
     sleep(1)
-    # servo.angle = 100
-    # sleep(1)
 
 ### left eye ###
 # eye left and right - 70 to 180 degrees - 125 center
@@ -25,7 +25,7 @@ while True:
 # top lid close and open - 125 to 35 degrees
 
 ### right eye ###
-# eye left and right - 70 to 180 degrees - 125 center
-# eye up and down - 90 to 140 degrees - 110 center
+# eye left and right - 60 to 160 degrees - 110 center
+# eye up and down - 110 to 160 degrees - 125 center
 # bottom lid close and open - 30 to 130 degrees
-# top lid close and open - 125 to 35 degrees
+# top lid close and open - 135 to 45 degrees
